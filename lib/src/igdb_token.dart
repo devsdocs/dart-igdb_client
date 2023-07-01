f555773e4207b2d01eac5f081f18c14191c4ef92
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-07-01 14:48:27 +0900

    Add fromJson() to IGDBToken

--- lib/src/igdb_token.dart
+++ lib/src/igdb_token.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 class IGDBToken {
   IGDBToken({this.accessToken, this.expiresIn, this.tokenType});
 
+  factory IGDBToken.fromJson(String str) =>
+      IGDBToken.fromMap(json.decode(str) as Map);
+
   factory IGDBToken.fromMap(Map map) => IGDBToken(
         accessToken: map['access_token'] as String?,
         expiresIn: map['expires_in'] as int?,
