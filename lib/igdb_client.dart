0e9cbd4968b51d353c3f984f51d1ae6833803eff
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-07-02 17:15:37 +0900

    Export Token Class

--- lib/igdb_client.dart
+++ lib/igdb_client.dart
@@ -1,6 +1,5 @@
-
-
 export 'package:igdb_client/src/enums/enums.dart';
 export 'package:igdb_client/src/igdb_client.dart';
 export 'package:igdb_client/src/igdb_helpers.dart';
+export 'package:igdb_client/src/igdb_token.dart';
 export 'package:igdb_client/src/request_parameters.dart';
